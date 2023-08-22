617e53ce8187d4063bdb0907c5a17acb6cb8c02f
saulocosta <saulo.costa@gmail.com>
2023-08-22 20:05:03 -0300

    Projeto Navegar entre Paginas HTML

---
+++ src/main/java/saulo/io/projetoNavegrPaginas/ProjetoNavegrPaginasApplication.java
@@ -0,0 +1,13 @@
+package saulo.io.projetoNavegrPaginas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNavegrPaginasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNavegrPaginasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saulo/io/projetoNavegrPaginas/ProjetoNavegrPaginasApplicationTests.java
@@ -0,0 +1,13 @@
+package saulo.io.projetoNavegrPaginas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNavegrPaginasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
